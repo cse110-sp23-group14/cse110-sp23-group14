cf2a18a8ac4fb69ace63c80835d03f669c33d834
wengthree1cm <chl135@ucsd.edu>
2023-06-02 00:19:28 -0700

    added database for lifepath number

--- source/scripts/birthday-numerology.js
+++ source/scripts/birthday-numerology.js
@@ -45,7 +45,7 @@ const soulUrgeNumberData = {
       You are mostly clear about the path of achievement but you also demand attention and love from people around. Often you get agitated when things do not go your way. The swollen ego, boastful nature and arrogance at times drive you insane and you hamper things around. 
         `,
 
-      "characteristics": 
+      "characteristic": 
       `Positive:
             As declared leaders, you own qualities like lateral thinking and creativity. 
             Others might perceive you as self-centred and egoistic, but your leadership and nut-cracking quality is what makes people envious of you. 
@@ -90,7 +90,7 @@ const soulUrgeNumberData = {
        You own the power to self-heal from hurtful situations and also specialize in helping others do the same. 
        People with this life path number are mostly in professions like spirituality, healing, nursing, etc.`,
 
-      "characteristics": 
+      "characteristic": 
       `Positive:
       Sensitivity and loyalty run in your blood. It makes you a true friend or partner. 
       You stand strong in any situation to protect your loved ones. You are open to listening and are compassionate to others' feelings. 
@@ -163,23 +163,168 @@ const soulUrgeNumberData = {
     },
 
     "4": {
-      "": "People with a Soul Urge Number 4 are practical, reliable, and hardworking.",
-      "description": "They have a strong desire for stability, order, and structure in their lives."
+      "peronality": `The fours are the emblem of stability and functional life. You are practical in either personal and professional front or any situation that demands your attention. 
+      You desire financial stability which is your topmost priority. The thirst for respect is much bigger than anything around. 
+      Companies love to have employees like you, who are team players and can hold the team together. You are imaginative, rational and anti any kind of injustice that takes place anywhere. 
+      You are trustworthy when it comes to emotional support and people can depend on you for any kind of assistance. Your seriousness and hard work are likely to take you a long way.`,
+
+      "characteristic": 
+      `Positive:
+
+      You are down to earth which makes you approachable and reachable for any kind of assistance. You are people's person that is why you are amongst the much need people in the core group. 
+      The best part about you is that you do not surrender to any kind of peer pressure. Perseverance is what you demand most of the time from self and even from people around you. 
+      The orderly form of life is your life long mantra. You are a learner and seek knowledge from everywhere you go. Risk bearing feature is absent in your system, 
+      you would rather make a solid backup plan and act accordingly than depend on taking the risk without a safety net at hand. 
+      You are determined in whatever you do and even possess loner quality, where you can enjoy a lonely time too.
+      
+      Negative:
+      
+      Unfortunately, you are very harsh on yourself. Either it is because of some old wounds which have yet not healed or any mishap that has left an unhealed scar. 
+      Such things make you feeble and you find it hard to forget. One of the biggest problems with you is that you are not ready to compromise in any situation. 
+      You are a staunch rule follower and expect others to have the same ideology which is quite impossible most times. 
+      If you find out people working or associated with you do not carry the same ideology, then you tend to lash out at them, you become impolite, 
+      stubborn and even authoritative which gives a negative impact on your image`,
+
+      "career":
+      `The fours are the kind of employees companies look for. You are focused, goal-oriented and super hard working. 
+      You can fix any situation irrespective of time, place or condition. As dedicated and reliable, your seniors are likely to bestow you with responsibilities. 
+      This is the reason your position graph is always moving upwards on the professional front.  You are a master in completing any task given to you. 
+      Whether it boring, monotonous or even time consuming, you do not give up on completing it. Others may give up after some time but you don't until the task is completed. 
+      This is the reason you turn out to be good trainers.`,
+
+      "love":
+      `Looking at the seriousness you keep regarding life, you are not a natural romantic. You are not the kind who can sweep his/her lover off their feet or plan a pomp surprise celebration for them. 
+      You cannot grant a fairy-tale moment to your partners this can be one reason you are considered a sloppy lover. 
+      Relationship for you is a serious genre that you deal with just like other aspects of your life. There is no special treatment from your end. 
+      You desire a steady relationship and a partner who is as hard-working as you.
+
+      Life path number 4 compatibility is mostly seen with 1 and 8. Since people who belong to 8 numbers are goal-oriented, they are likely to gel well with the fours. `
     },
 
     "5": {
-      "meaning": "Individuals with a Soul Urge Number 5 are adventurous, freedom-loving, and adaptable.",
-      "description": "They have a strong desire for freedom, variety, and experiences."
+      "personality": `You are an explorative personality who loves to explore. You adore your freedom and ensures that nobody dares to take it away from you. 
+      Traveling, meeting new people, enjoying even the smallest experience is what describes you. Curiosity runs in your blood, it pushes you to touch new heights.
+      You are also a risk- taker but a calculated one. The risk is worth the leap is what you always make sure. 
+      The adventure-seeking nerve makes you a good motivator to push people to explore their potential. Fearlessness is what you constantly aim to conquer, 
+      as it restricts your flight to fly high and make the most of what is around you. Change is the constant factor in your life that you love the most. 
+      Monotony bores you and you make sure to exit any relation or situation that cradles it.`,
+
+
+      "characteristic": 
+      `Positive:
+
+      You are full of ideas, which helps to enhance your overall personality and also image amongst people. You inspire people both on the professional and personal front. 
+      Since you are young at heart, you do not support orthodox ideologies and rules. Live life to the fullest without any restrictions is what you advise people all the time. 
+      You possess good communication skills this helps you crack some good deals and even expand your social circle. 
+      Progressive thinking is the tool that makes you live life in the highest way possible without any judgments.
+      
+      Negative:
+      As a person who is never at rest, you often end up with unfinished tasks in your basket. Since you are too hyper with all the workaround, 
+      you just aim at doing everything at once leading to no work done. Lack of concentration makes you agitated and lose cool. 
+      You are anxious most of the time, and anger always sits on your nose, if things do not go your way. 
+      The rebellious side of you turns to active mode when someone tries to tie you or take away your freedom. 
+      You are inclined towards making a lot of enemies, ensure you keep yourself away from such situations. Stay away from fights as much as possible to preserve peace in life`, 
+
+      "career":`Committing to a career is very difficult for the fives as they fear to be tied. You are a powerhouse of energy, ideas, 
+      and passion and if you realize that the job does not give justice to your expectation, you tend to run away from such profiles. 
+      Your fluctuating mind and restlessness often restrict you from taking up new or long projects, as you are doubtful about your stability. 
+      You very well know that excitement or zeal for something does not stay the same in you over time. You seek perfection but lack commitment and prolonged concentration,
+       this is the reason you feel directionless most of the time. The best part about fives is that you can develop new skills in any field, so the right flexibility and liking, 
+       this is the only way you can hold yourself and settle to find a purpose in life.`,
+
+       "love":`Your free-spiritedness is one of the charming facts about you that attracts people towards you. Even though you are shy about romance, you very well know what you seek. 
+       The fear of being pulled by your partner into his/ her style of living, you avoid any kind of commitment and serious relationships. 
+       Since freedom is what you love the most, you are not ready to compromise even if it costs keeping the love of your life at one arm distance.
+
+       Life path number 5 compatibility is mostly seen with the same life path number people. As you both love your born free nature and understand each other's plight, you enjoy a relationship together. 
+       You will get along well with 1 and 7 too, as your energy will match with them and you would enjoy exploring and traveling with them`
     },
 
     "6": {
-      "meaning": "People with a Soul Urge Number 6 value harmony, family, and nurturing.",
-      "description": "They have a deep desire to care for and support others."
+      "personality": 
+      `You are natural caretakers and are ready to go to any limit to keep your closed ones happy and content. 
+      Your sacrificing nature makes you a people's person and also one of the most naïve personality. Your motherly side often makes you choose careers like teaching, training, childcare, etc. 
+      Your emotionally intuitive nature makes you super kind that is taken for granted by some often. 
+      Often you lack to see the reality of people and when hurt you still do not believe and you end up being hurt or betrayed. 
+      Your escape from harsh reality is a drawback you should overcome. You love to take up responsibilities and make sure to do justice to the same. 
+      Your humility is a gem on your personality crown.`,
+
+      "characteristic": 
+      `Positive:
+      The empathetic nature gives you a big heart to understand each one's point of view and feelings. 
+      You prefer keeping others comfort before yours, this is the reason you do not think about yourself at all. 
+      Your sacrificing nature, makes you most wanted amongst family and friends. At times, the extent of sacrifice and the desperation to make the other person feel good has no limit, 
+      which eventually affects your overall health and can exhaust you completely. Love, peace, and support are inbuilt emotions in you making you appear charming and the 
+      most warm-natured person to be with.
+      
+      Negative:
+      
+      They say too much of everything is bad! Such is the case with the life path number 6. Your over-servicing nature makes you ignore yourself and results in self-destruction. 
+      By default, you assume that you are obliged to do things for others and keep your preferences at bay. Due to this, 
+      you do not get a chance to exhibit your talent and skills that might benefit you professionally and personally. To put it in harsh words, you do not mind being slaves to people.
+       With all your energy channelized towards others betterment, you are likely to get sick worrying and eventually you are unable to showcase your talent, as you avoid taking hard tasks.`,
+
+     "career":
+     `You lack self-confidence and are mostly the back-bencher in all the fields, courtesy you‘re giving nature. On the contrary, you are the one who spreads a cozy vibe at work. 
+     You are an employee who is always ready to contribute as much as you can. This is the reason most of you opt for professions like social work, teaching, training, etc. 
+     You can excel in the leadership position, as you are someone who ensures peace and harmony along with the betterment of your fellow members. 
+     You are a control freak, a problem solver with a cool mind, this is the reason your team is always disciplined and happy at the same time. 
+     Your decision-making skills work well. You are a soft-spoken person, this gives you a chance to show your talent and skills to others around.`,
+
+     "love":
+     `Reading all the above qualities, it is quite evident that you are an amazing partner when it comes to relationships. 
+     Your caring, selfless nature makes the partner comfortable, as they get all the attention and the right to make decisions.
+      Your motherly nature does not only confined to the partner, but you are also the same when it comes to your family. 
+      They are the most important people to you and their happiness is what you seek all the time. Even as a peacemaker and the ocean of love, 
+      you are very careful and observant about who will be your partner. You take ample time to choose and analyze all the aspects. When you are satisfied then only you pick the right one for you.
+
+     Life path number 6 compatibility is best seen with 1, 2 and 9. 1 and 9 seek guidance and direction in life to achieve success. 
+     6s are a pro at providing that and thus they make a good combination`
     },
 
     "7": {
-      "meaning": "Individuals with a Soul Urge Number 7 are introspective, analytical, and spiritual.",
-      "description": "They have a strong desire for knowledge, wisdom, and inner growth."
+      "personality": 
+      `Life path number 7 is always thirsty for knowledge. Your basic aim of life is to explore your surroundings and develop both spiritually and mentally. 
+      You are a seeker of reality and are on the lookout to attain wisdom. In some way, you are also a perfectionist, 
+      which might make your relation with people uneasy as perfection that you seek is only in your mind. You often lookout for flaws in people which does let you earn a good reputation. 
+      You set very high standards for self, which eventually makes you tough within. You are a loner and often like to escape to your cocoon for a solace. 
+      You might appear weird when you try to overdo things and appear normal or over-friendly, as you are a reserved man. 
+      Such an effort might backfire and make people look down at you as vulgar and judgmental. 
+      You love your lonely mental place and most of the time are busy finding answers about your life and its purpose.`,
+
+      "characteristic": 
+      `Positive:
+
+      You prefer to work in solitude and figure out things alone without anyone's help. You have your own beliefs and you like to stick to them all the time. 
+      It is very difficult to change your mindset or decisions. Even though you are a loner, 
+      you have a magnetic charm that attracts people every time you decide to walk out of your shell are confident to express yourself. 
+      At times people see a humorous side of you which they like. You are sensible and sensitive at the same time. 
+      You have infinite spiritual energy in you that helps you to evolve, know yourself and also introspect various aspects of life.
+    
+      Negative:
+      
+      Your loner side is not what people close to you enjoy. You often judge others because they do not have a mind-set like you or cannot do things just the way you like. 
+      Paranoia runs in your blood and is also the reason you are likely to collapse. 
+      The tiff between heart and mind is never-ending as you want to ignore your heart completely and follow your mind which is not possible. 
+      Since you are on the lookout for inner peace, you fail to realize that it is right there and your mindset is the only barrier that restricts you from attaining it. 
+      You are a secretive person and someone who conceals his feelings easily, thus making it difficult to know what exactly you are feeling and feeding your mind. 
+      A cruel critic, slap tongue, careless use of words are few of your not so good sides. You love to escape from reality and often appear is a person who is not understanding. 
+      Unfortunately, you can be exhausting to be around.`,
+
+      "career":
+      `You are a learner and also a wise man at the same time at work. You enjoy learning practically than theoretically. 
+      Intuitiveness is one of your qualities. You have excellent communication skills and are a good guide to others. 
+      Since you are intelligent and observant you are likely to take up professions like scientists, investigation, philosophy, etc. 
+      You enjoy working independently. Comfort takes a back seat when it comes to focusing at work and completing the task. 
+      In spite of that, you are anti-deadlines. You have your way of working which you do not like to change when others advise you to follow a different way of working. `,
+
+      "love":
+      `You tend to fall in love quickly if you ignore the bug of finding self. At times the blind run for personal quest makes your partner feel out of place, lonely and unwanted. 
+      You as a lover you always want reassurance in the relationship. At times, to make the condition right you can overdo love declaration and gestures which might make your partner feel awkward. 
+      Winning your trust is a tedious task. You love natural beauty and prefer to cradle yourself in the same- close to nature. 
+      You are a reserved person and yet you are responsible for all the worrying and hampering the peaceful ambiance.
+
+      Life path number 7 compatibility is highest with 5 and 3. As they will always challenge you and never bore you.`
     },
 
     "8": {
