2141f24f7bfafa92c8c46d7ec6afc70373251022
Mive <luoyulin9103@gmail.com>
2023-06-06 22:36:36 -0700

    fix settings.js style

--- source/scripts/settings.js
+++ source/scripts/settings.js
@@ -73,8 +73,7 @@ function setHoroscopeBackground() {
 
     if (zodiacSign) {
         const imageKey = zodiacSign.charAt(0).toUpperCase() + zodiacSign.slice(1);
-        horoscopeContent.style.backgroundImage = `url(${constellation[imageKey]})`;
-    }
+        horoscopeContent.style.backgroundImage = `url(${constellation[imageKey]})`;}
     else {
         horoscopeContent.style.backgroundImage = `url(${defaultImage})`;
     }
