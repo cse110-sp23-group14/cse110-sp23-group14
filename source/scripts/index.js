00832d0a8edeca1eee72f86e9ac660294788b940
Combobyte <varun.sreedhar1@gmail.com>
2023-06-11 12:20:19 -0700

    fixed bug where there no scoll on compatability

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -250,6 +250,16 @@ window.addEventListener('DOMContentLoaded', async (event)=> {
     });
 
     /**
+     * Scrolls to the compatability page
+     * @param {Event} event - "click"
+     */
+    document.getElementById('compatibility-link').addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        scrollToElement(target);
+    });
+
+    /**
      * Scrolls to the FAQ page
      * @param {Event} event - "click"
      */
