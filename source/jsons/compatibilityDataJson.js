8c8f0da4946aa2ac041dc791971eecc4584fd16b
Combobyte <varun.sreedhar1@gmail.com>
2023-06-10 22:20:39 -0700

    fixed style on compatibility json

--- source/jsons/compatibilityDataJson.js
+++ source/jsons/compatibilityDataJson.js
@@ -5,17 +5,17 @@ const compatibilityData = {
     "compatibility": [
         {
             "pair": [
-                ["Aries", "Aries"], 
-                ["Taurus", "Taurus"], 
-                ["Gemini", "Gemini"], 
-                ["Cancer", "Cancer"], 
-                ["Leo", "Leo"], 
-                ["Virgo", "Virgo"], 
-                ["Libra", "Libra"], 
-                ["Scorpio", "Scorpio"], 
-                ["Sagittarius", "Sagittarius"], 
-                ["Capricorn", "Capricorn"], 
-                ["Aquarius", "Aquarius"], 
+                ["Aries", "Aries"],
+                ["Taurus", "Taurus"],
+                ["Gemini", "Gemini"],
+                ["Cancer", "Cancer"],
+                ["Leo", "Leo"],
+                ["Virgo", "Virgo"],
+                ["Libra", "Libra"],
+                ["Scorpio", "Scorpio"],
+                ["Sagittarius", "Sagittarius"],
+                ["Capricorn", "Capricorn"],
+                ["Aquarius", "Aquarius"],
                 ["Pisces", "Pisces"]
             ],
 
