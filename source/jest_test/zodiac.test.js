e20ae447054c00ce779cfc8745be31a80fd1458e
Combobyte <varun.sreedhar1@gmail.com>
2023-06-11 19:46:37 -0700

    added more jest tests

--- source/jest_test/zodiac.test.js
+++ source/jest_test/zodiac.test.js
@@ -16,8 +16,23 @@ describe('getZodiacSign', () => {
         expect(zodiacSign).toBe('Taurus');
     });
   
-    it('should return null if no zodiac sign is found for the given month and day', async () => {
-        const zodiacSign = await Zodiac.getZodiacSign(2, 15);
-        expect(zodiacSign).toBe('Aquarius');
+    it('should return the correct zodiac sign for a given month and day at the end range', async () => {
+        const zodiacSign = await Zodiac.getZodiacSign(7, 25);
+        expect(zodiacSign).toBe('Leo');
+    });
+
+    it('should return the correct zodiac sign for a given month and day at the end range', async () => {
+        const zodiacSign = await Zodiac.getZodiacSign(11, 1);
+        expect(zodiacSign).toBe('Scorpio');
+    });
+
+    it('should return the correct zodiac sign for a given month and day at the end range', async () => {
+        const zodiacSign = await Zodiac.getZodiacSign(12, 4);
+        expect(zodiacSign).toBe('Sagittarius');
+    });
+
+    it('should return the correct zodiac sign for a given month and day at the end range', async () => {
+        const zodiacSign = await Zodiac.getZodiacSign(6, 1);
+        expect(zodiacSign).toBe('Gemini');
     });
 });
